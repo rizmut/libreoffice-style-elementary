4e38510c00746f84dc11925a85ec703fc7ab86a1
Rizal Muttaqin <riz_17_oke@yahoo.co.id>
2021-01-06 06:24:08 +0700

    tdf#139056 update GREEN color to follow upstream brand

--- update-green-color-brand.sh
+++ update-green-color-brand.sh
@@ -15,4 +15,16 @@ do
     sed -i -e 's/#85f619/#d1ff82/g' "$i" #green8 light
     sed -i -e 's/#7bb339/#206b00/g' "$i" #edge green
     sed -i -e 's/#aff05f/#9bdb4d/g' "$i" #light green
+    sed -i -e 's/#425a2d/#206b00/g' "$i" #edge green (mimetypes)
+    sed -i -e 's/#7f956d/#3a9104/g' "$i" #edge green (mimetypes)
+    sed -i -e 's/#b5cc85/#d1ff82/g' "$i" #green1 (mimetypes)
+    sed -i -e 's/#9abe74/#9bdb4d/g' "$i" #green2 (mimetypes)
+    sed -i -e 's/#669f4d/#68b723/g' "$i" #green3 (mimetypes)
+    sed -i -e 's/#47892b/#3a9104/g' "$i" #green4 (mimetypes)
+    sed -i -e 's/#bdff69/#d1ff82/g' "$i" #green1 (mimetypes2)
+    sed -i -e 's/#8ed451/#9bdb4d/g' "$i" #green2 (mimetypes2)
+    sed -i -e 's/#58aa30/#68b723/g' "$i" #green3 (mimetypes2)
+    sed -i -e 's/#2a6c1f/#3a9104/g' "$i" #green4 (mimetypes2)
+    sed -i -e 's/#b7d095/#9bdb4d/g' "$i" #green2 (mimetypes2)
+    
 done
